a8e58b77d46d624a5a51672b430329703edacd84
Lincoln de Sousa <lincoln@comum.org>
2013-06-06 16:01:52 -0400

    Add `Environment.from_folder()` to mimic envdir
    Now our software is able to use the same format that `envdir` uses to
    load folders where each file is a variable and the content of the file
    is the value.
    
    It's also possible to delete stuff from the environment too. If you do
    something like this:
    
        >>> env = Environment.from_folder('/etc/envdir/yipit')
        >>> del env['KEY']
    
    It will actually delete the file called `KEY` inside of the environment
    path pointed in the constructor.

--- envparse/__init__.py
+++ envparse/__init__.py
@@ -1,7 +1,40 @@
+from __future__ import unicode_literals
+
 import io
 import os
 import urlparse
 import yaml
+
+
+class FolderStorage(dict):
+    def __init__(self, path):
+        self.path = path
+
+    def _open(self, name, mode='r'):
+        return io.open(os.path.join(self.path, name), mode)
+
+    def get(self, name, failobj=None):
+        try:
+            # If we need to optimize the amount of IO calls we make over the
+            # time, we just have to cache the modified data when we open the
+            # file and compare with the current time when we need the file
+            # again.
+            super(FolderStorage, self).__setitem__(
+                name, self._open(name).read().strip())
+            return self[name]
+        except IOError:
+            return failobj
+
+    def __setitem__(self, name, value):
+        super(FolderStorage, self).__setitem__(
+            name, self._open(name, 'w').write(unicode(value)))
+
+    def __delitem__(self, name):
+        os.unlink(os.path.join(self.path, name))
+        super(FolderStorage, self).__delitem__(name)
+
+    def items(self):
+        return [(i, self.get(i)) for i in os.listdir(self.path)]
 
 
 class Environment(object):
@@ -13,6 +46,13 @@ class Environment(object):
     @classmethod
     def from_file(cls, path):
         return cls(storage=yaml.load(io.open(path)))
+
+    @classmethod
+    def from_folder(cls, path):
+        return cls(storage=FolderStorage(path))
+
+    def __delitem__(self, name):
+        del self.storage[name]
 
     def items(self):
         return self.storage.items()
@@ -30,7 +70,7 @@ class Environment(object):
         return float(self.get(name, failobj))
 
     def get_bool(self, name, failobj=None):
-        val = str(self.get(name, failobj)).lower()
+        val = unicode(self.get(name, failobj)).lower()
         try:
             return bool(int(val))
         except ValueError:
--- tests/functional/test_envparse.py
+++ tests/functional/test_envparse.py
@@ -10,3 +10,43 @@ def test_envparse_environment_from_file():
     # When I try to find a variable defined in that file, then I see that it
     # works
     env.get('FAVORITE_SUPER_HERO').should.equal('Batman NANANANANA')
+
+
+def test_envparse_environment_from_directory():
+    # Given that I load variables to my env from a folder
+    env = Environment.from_folder(
+        os.path.join(os.path.dirname(__file__), './fixtures/env'))
+
+    # When I try to list all the variables inside of that folder
+    sorted(env.items(), key=lambda x: x[0]).should.equal([
+        ('ENABLE_SOMETHING', u''),
+        ('PI', u'3.14'),
+        ('SERVER_URI', u'smtp://user@mserver.com:passwd@mserver.com:25'),
+    ])
+
+    # When I try to find the variables, then I see they're there correctly
+    env.get_bool('ENABLE_SOMETHING').should.be.false
+    env.get_bool('ENABLE_SOMETHING_ELSE', True).should.be.true
+    env.get_float('PI').should.equal(3.14)
+    env.get_uri('SERVER_URI').host.should.equal('mserver.com')
+    env.get_uri('SERVER_URI').user.should.equal('user@mserver.com')
+
+
+def test_envparse_environment_from_directory_set():
+    # Given that I load variables to my env from a folder
+    path = os.path.join(os.path.dirname(__file__), './fixtures/env')
+    env = Environment.from_folder(path)
+
+    # When I set a new variable to my folder env
+    env.set('CITY', 'NEW-YORK')
+
+    # Then I see the file was created with the right content
+    target = os.path.join(path, 'CITY')
+    os.path.exists(target).should.be.true
+
+    # And then I see that the value is also right
+    open(target).read().should.equal('NEW-YORK')
+
+    # And then I see that after removing the item, the file will also go away
+    del env['CITY']
+    os.path.exists(target).should.be.false
